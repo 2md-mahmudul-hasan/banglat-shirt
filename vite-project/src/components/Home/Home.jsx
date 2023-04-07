2d87b67da2697975868021fdd61d15883b6ab048
2md-mahmudul-hasan <2md.mahmudul.hasan@gmail.com>
2023-04-07 16:55:57 +0600

    home component

--- vite-project/src/components/Home/Home.jsx
+++ vite-project/src/components/Home/Home.jsx
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
+import Tshirt from '../Tshirt/Tshirt';
+import Cart from '../Cart/Cart';
+import './home.css'
 
 const Home = () => {
   const tshirts = useLoaderData()
+  const [cart, setCart] = useState([])
+
+  const handleCart = (tshirt) => {
+    console.log(tshirt)
+  }
   return (
-    <div>
-      I am in home{tshirts.length}
+    <div className="home-container">
+      <div className='tshirt-container'>
+        {
+          tshirts.map(tshirt => <Tshirt handleCart={handleCart} tshirt={tshirt} key={tshirt._id}></Tshirt>)
+        }
+      </div>
+
+      <div className="cart-container">
+        <Cart></Cart>
+      </div>
     </div>
+
   );
 };
 
